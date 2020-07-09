ca378d3acdc49502cde5d1a607e99a0daa6dd943
Michael Shepherd <mwshepherd11@gmail.com>
2020-07-09 16:05:22 +1000

    user auth, knock, endpoints

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_061003) do
+ActiveRecord::Schema.define(version: 2020_07_09_054753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2020_07_08_061003) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "longitude"
     t.float "latitude"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "cities", "countries"
