1ef3dda7111dcd65a16a6d534b7d13d9cb432717
Michael Shepherd <mwshepherd11@gmail.com>
2020-07-09 14:21:14 +1000

    delete endpoint

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,10 +1,13 @@
 class CountriesController < ApplicationController
+    before_action :set_country, only: [:destroy]
+
     def index
         @countries = Country.all
         if params[:type] == "json"
             data = @countries.map do |country|
                 {
                     name: country.name,
+                    id: country.id,
                     coords: [country.latitude, country.longitude]
                 }
             end 
@@ -18,7 +21,16 @@ class CountriesController < ApplicationController
         render json: "country bookmark", status: 200
     end
 
+    def destroy
+        @country.destroy
+        render json: 'country deleted', status: 200
+    end
+
     private
+
+    def set_country
+        @country = Country.find(params[:id])
+    end
 
     def country_params
         params.require(:country).permit(:name)
