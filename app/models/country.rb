473f0d9129b6877b26e78db0109d845d76ce7a6d
Michael Shepherd <mwshepherd11@gmail.com>
2020-07-10 11:24:39 +1000

    added countries to users, seeds file and authenticate users before actions

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,5 @@
 class Country < ApplicationRecord
+    belongs_to :user
     has_many :cities
     geocoded_by :name
     after_validation :geocode
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_054753) do
+ActiveRecord::Schema.define(version: 2020_07_10_002502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2020_07_09_054753) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "longitude"
     t.float "latitude"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_countries_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +44,5 @@ ActiveRecord::Schema.define(version: 2020_07_09_054753) do
   end
 
   add_foreign_key "cities", "countries"
+  add_foreign_key "countries", "users"
 end
